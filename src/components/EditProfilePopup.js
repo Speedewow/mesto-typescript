e8451c9ecd864f4f5a837d08f462e2774e06469e
Speede <Speedew0w@yandex.ru>
2022-06-24 09:41:32 +0300

    Final ver. 10 project

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -2,7 +2,7 @@ import Input from './Input';
 import PopupWithForm from './PopupWithForm';
 export default function EditProfilePopup({ isOpen, onClose }) {
 	return (
-		<PopupWithForm isOpen={isOpen} onClosePopup={onClose} name="profile" title="Редактировать профиль">
+		<PopupWithForm isOpen={isOpen} onClose={onClose} name="profile" title="Редактировать профиль" buttonText="Сохранить">
 			<Input className="profile-name" name="name" placeholder="Введите имя" type="text" />
 			<Input className="profile-info" name="info" placeholder="Введите профессию" type="text" />
 		</PopupWithForm>
